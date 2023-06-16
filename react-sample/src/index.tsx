0a5f8f009a72556422a0225f65e7cc4385b47904
mimunojun <mimura.tsukuba@gmail.com>
2023-06-17 00:28:56 +0900

    component ordered

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { UseContextSample } from './components/UseContextSample';
+import { UseContextSample } from './components/354_UseContextSample';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
