080d39cb72eaf998adc0da520ff1b11a4099bea2
mimunojun <mimura.tsukuba@gmail.com>
2023-06-27 20:40:57 +0900

    styled-components theme

---
+++ next-sample2/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './theme'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
+}
+
+export default MyApp
